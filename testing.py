16c9f95c40268b77542d9e78e3bce1c0276bb66a
calzoom <calzoom@berkeley.edu>
2019-10-06 19:32:44 -0700

    card init tests

--- testing.py
+++ testing.py
@@ -7,12 +7,13 @@ from helper import Card
 
 class TestStringMethods(unittest.TestCase):
     def test_rank_suit_vals(self):
-        self.assertRaises(ValueError, Card, 14, "Spades")
-        self.assertRaises(ValueError, Card, None, "Spades")
-        self.assertRaises(ValueError, Card, "fourteen", "Spades")
-        self.assertRaises(ValueError, Card, 14, "Kings")
+        self.assertRaises(ValueError, Card, 14, "Spade")
+        self.assertRaises(ValueError, Card, None, "Spade")
+        self.assertRaises(ValueError, Card, "fourteen", "Spade")
+        self.assertRaises(ValueError, Card, 14, "King")
         self.assertRaises(ValueError, Card, 6, None)
         self.assertRaises(ValueError, Card, None, 29)
+        print("Card Init: PASSED")
 
 if __name__ == "__main__":
     print("Running: Testing.py -----")
